3e7540d55c78862d10dd73d34b849f70e35bff6f
FFusa1 <khachidzedimitri@gmail.com>
2024-10-21 16:17:32 +0400

    scroller

---
+++ script.js
@@ -0,0 +1,20 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+if(!windows.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller =>{
+        scroller.setAttribute("data-animated", true)
+
+        const scrollerInner = scroller.querySelector('.scroller__inner');
+        const scrollerContent = array.from(scrollerInner.children);
+    
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden',true)
+            scrollerInner.appendChild(duplicatedItem);
+        })
+    })
+}
